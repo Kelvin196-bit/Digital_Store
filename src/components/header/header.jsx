41f00b839cfb1a660c9c2eb98d2f004529a293b0
kelv <kelvalves.4@gmail.com>
2025-05-10 13:15:27 -0300

    segundo commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import './header.css'
+import NavBar from './header_components/nav_bar';
+
+const Header = () => {
+    return (<header className='principal'>
+        <NavBar/>
+    </header>);
+}
+
+export default Header;
